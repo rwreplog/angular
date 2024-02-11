34d6dbe906a8842dfcbc9e9f390618a6da7d10a9
Ryan Replogle <ryan.replogle@gmail.com>
2024-02-10 19:42:51 -0500

    refactor

---
+++ projects/angular/src/app/features/admin/admin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.scss']
+})
+export class AdminComponent {
+
+}
---
+++ projects/angular/src/app/shared/shared.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { LoaderComponent } from './loader/loader.component';
+
+
+@NgModule({
+  declarations: [
+    LoaderComponent
+  ],
+  imports: [
+    // vendor
+    CommonModule,
+    RouterModule,
+
+    //material
+    MatCardModule,
+    MatButtonModule,
+  ],
+  exports: [
+    //vendor
+    CommonModule,
+    RouterModule,
+
+    //material
+    MatCardModule,
+    MatButtonModule,
+
+    //local
+  ]
+})
+export class SharedModule { }
